a0a3a01716cec9ab114c16c3260acd75fd372343
eugeniogrigoras <eugeniogrigoras@gmail.com>
2016-02-22 17:53:02 +0100

    lklkl

--- login.php
+++ login.php
@@ -51,14 +51,15 @@
 <div class="centered" style="margin-top:24px">
     <div class="vertical-section" style="padding:0;">
         <div class="title" style="padding:24px">Login</div>
-        <form style="padding:24px;" is="iron-form" id="formGet" method="get" action="/">
+        <form style="padding:24px;" is="iron-form" id="formPost" action="ciao.php" method="post" enctype="multipart/form-data">
             <gold-email-input required auto-validate error-message="Please enter a valid email!" label="Email contact"></gold-email-input>
             <paper-input name="password" label="Password" type="password" required></paper-input>
             <br>
             <paper-checkbox checked class="grey">Remember me</paper-checkbox>
             <br><br>
             <div class="submit-button-container">
-                <paper-button onclick="submitHandler(event)">Login</paper-button>
+                <paper-button id="paper-button" onclick="submitForm()">Login</paper-button>
+                <button type="submit" id="SubmitButton" name="submit" style="display:none"></button>
             </div>
         </form>
         <hr style="margin:0;">
@@ -72,5 +73,12 @@
 <!-- Fine Sign In  -->
 
 <?php require 'footer.php';?>
+
+<script>
+    function submitForm(){
+        document.getElementById('SubmitButton').click();
+        console.log("Login!");
+    }
+</script>
 
 <?php require 'close.php';?>
